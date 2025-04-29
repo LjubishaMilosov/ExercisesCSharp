404b6352d1f17132ddb0bfc5a39a5f35a0d24939
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-04-29 16:31:11 +0800

    if exercise1

---
+++ Code_Class02/Code/ifExercises/Program.cs
@@ -0,0 +1,34 @@
+﻿//Get an input number from the console
+//Print all numbers from 1 up to that number
+//Get another input number from the console
+//Print all numbers starting from that number up to 1
+
+Console.WriteLine("Enter a number:");
+bool firstSuccess = int.TryParse(Console.ReadLine(), out int firstNumber);
+
+if (firstSuccess)
+{
+    for (int i = 1; i <= firstNumber; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input. Please enter a valid number.");
+}
+
+Console.WriteLine("Enter another number:");
+bool secondSuccess = int.TryParse(Console.ReadLine(), out int secondNumber);
+if (secondSuccess)
+{
+    for (int i = secondNumber; i >= 1; i--)
+    {
+        Console.WriteLine(i);
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input. Please enter a valid number.");
+}
+

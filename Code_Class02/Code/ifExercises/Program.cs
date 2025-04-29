46c2e545f03a9438253db788b1e149e64b94d20c
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-04-29 16:35:41 +0800

    if exercise2

--- Code_Class02/Code/ifExercises/Program.cs
+++ Code_Class02/Code/ifExercises/Program.cs
@@ -32,3 +32,29 @@ else
     Console.WriteLine("Invalid input. Please enter a valid number.");
 }
 
+
+//Get an input number from the console
+//Print all even numbers starting from 2
+//Get another input number from the console
+//Print all odd numbers starting from 1
+
+Console.WriteLine("Enter a number:");
+bool thirdSuccess = int.TryParse(Console.ReadLine(), out int thirdNumber);
+if (thirdSuccess)
+{
+    for (int i = 2; i <= thirdNumber; i += 2)
+    {
+        Console.WriteLine(i);
+    }
+    for (int i = 2; i <= thirdNumber; i++) //second way, with the classic check for even numbers
+    {
+        if (i % 2 == 0)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input. Please enter a valid number.");
+}
